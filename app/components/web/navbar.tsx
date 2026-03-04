66fc31b9325ab3afef00d3efe0cc2ffbcd670522
AniGh <ano.gambashidze@gmail.com>
2026-03-04 17:15:57 +0400

    add navbar depend on tutorial

---
+++ app/components/web/navbar.tsx
@@ -0,0 +1,35 @@
+import { Button, buttonVariants } from "@/components/ui/button"
+import Link from "next/link"
+
+export function Navbar() {
+  return (
+    <nav className="w-full py-5 flex items-center justify-between">
+        <div className="flex items-center gap-8">
+           <Link  href="/" >
+           <h1 className="text-3xl font-bold">Next
+                <span className="text-blue-500"> Pro</span>
+                </h1> 
+                </ Link>
+
+                <div className="flex flex-center gap-2">
+                    <Button asChild variant="ghost">
+                    <Link href="/" >Home </Link>
+                    </Button>
+                    <Button asChild variant="ghost">
+                    <Link href="/blog" >Blog</Link>
+                    </Button>
+                    <Button asChild variant="ghost">
+                    <Link href="/create" >Create</Link>
+                    </Button>
+                </div>
+                </div>
+                <div className="flex items-center gap-2">
+                    <Button asChild>
+                    <Link  href="/auth/sign-up">Sign Up</Link>
+                    </Button>
+                    <Button variant="outline" asChild>
+                    <Link href="/auth/Login">Login</Link>
+                    </Button>
+                </div>
+    </nav>
+  )}           
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Navbar } from "./components/web/navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,8 +26,11 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
+        <main className="max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-8">
+      <Navbar />
         {children}
+        </main>
       </body>
     </html>
   );
